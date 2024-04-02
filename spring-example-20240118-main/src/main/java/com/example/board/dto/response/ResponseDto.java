31a62837dce51c688ff7602a9ee908e7ac717dc7
pjh753156a <00-g-00@naver.com>
2024-04-02 17:46:50 +0900

    2024-04-02-50days

---
+++ spring-example-20240118-main/src/main/java/com/example/board/dto/response/ResponseDto.java
@@ -0,0 +1,25 @@
+package com.example.board.dto.response;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ResponseDto {
+    private String code;
+    private String message;
+    
+    public static ResponseEntity<ResponseDto> databaseError() {
+        ResponseDto body = new ResponseDto(ResponseCode.DATABASE_ERROR, ResponseMessage.DATABASE_ERROR);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(body);
+    }
+
+    public static ResponseEntity<ResponseDto> notExistUser() {
+        ResponseDto body = new ResponseDto(ResponseCode.NOT_EXIST_USER, ResponseMessage.NOT_EXIST_USER);
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(body);
+    }
+
+}
---
+++ spring-example-20240118-main/src/main/java/com/example/board/service/UserService.java
@@ -0,0 +1,11 @@
+package com.example.board.service;
+
+import org.springframework.http.ResponseEntity;
+
+import com.example.board.dto.response.user.GetUserResponseDto;
+
+public interface UserService {
+    
+    ResponseEntity<? super GetUserResponseDto> getUser(String email);
+
+}
